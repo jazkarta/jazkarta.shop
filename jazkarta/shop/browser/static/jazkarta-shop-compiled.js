28376d6c4ec0dec4617561235ea5f67ff940671a
witekdev <witekdev@gmail.com>
2017-05-04 14:47:27 -0500

    recompile the P5 css and js, manually fix some css

--- jazkarta/shop/browser/static/jazkarta-shop-compiled.js
+++ jazkarta/shop/browser/static/jazkarta-shop-compiled.js
@@ -9,16 +9,17 @@ define('mockup-utils',[
 
   var QueryHelper = function(options) {
     /* if pattern argument provided, it can implement the interface of:
-      *    - browsing: boolean if currently browsing
-      *    - currentPath: string of current path to apply to search if browsing
-      *    - basePath: default path to provide if no subpath used
-      */
+     *    - browsing: boolean if currently browsing
+     *    - currentPath: string of current path to apply to search if browsing
+     *    - basePath: default path to provide if no subpath used
+     */
 
     var self = this;
     var defaults = {
       pattern: null, // must be passed in
       vocabularyUrl: null,
       searchParam: 'SearchableText', // query string param to pass to search url
+      pathOperator: 'plone.app.querystring.operation.string.path',
       attributes: ['UID', 'Title', 'Description', 'getURL', 'portal_type'],
       batchSize: 10, // number of results to retrive
       baseCriteria: [],
@@ -27,6 +28,7 @@ define('mockup-utils',[
       pathDepth: 1
     };
     self.options = $.extend({}, defaults, options);
+
     self.pattern = self.options.pattern;
     if (self.pattern === undefined || self.pattern === null) {
       self.pattern = {
@@ -40,12 +42,14 @@ define('mockup-utils',[
     } else if (self.pattern.vocabularyUrl) {
       self.options.vocabularyUrl = self.pattern.vocabularyUrl;
     }
-    if (self.options.vocabularyUrl !== undefined &&
-        self.options.vocabularyUrl !== null) {
-      self.valid = true;
-    } else {
-      self.valid = false;
-    }
+    self.valid = Boolean(self.options.vocabularyUrl);
+
+    self.getBatch = function(page) {
+      return {
+        page: page ? page : 1,
+        size: self.options.batchSize
+      };
+    };
 
     self.getCurrentPath = function() {
       var pattern = self.pattern;
@@ -59,7 +63,7 @@ define('mockup-utils',[
       } else {
         currentPath = pattern.currentPath;
       }
-      if (typeof currentPath  === 'function') {
+      if (typeof currentPath === 'function') {
         currentPath = currentPath();
       }
       var path = currentPath;
@@ -75,17 +79,17 @@ define('mockup-utils',[
       return path;
     };
 
-    self.getCriterias = function(term, options) {
-      if (options === undefined) {
-        options = {};
+    self.getCriterias = function(term, searchOptions) {
+      if (searchOptions === undefined) {
+        searchOptions = {};
       }
-      options = $.extend({}, {
+      searchOptions = $.extend({}, {
         useBaseCriteria: true,
         additionalCriterias: []
-      }, options);
+      }, searchOptions);
 
       var criterias = [];
-      if (options.useBaseCriteria) {
+      if (searchOptions.useBaseCriteria) {
         criterias = self.options.baseCriteria.slice(0);
       }
       if (term) {
@@ -96,57 +100,21 @@ define('mockup-utils',[
           v: term
         });
       }
-      if(options.searchPath){
+      if (searchOptions.searchPath) {
         criterias.push({
           i: 'path',
-          o: 'plone.app.querystring.operation.string.path',
-          v: options.searchPath + '::' + self.options.pathDepth
+          o: self.options.pathOperator,
+          v: searchOptions.searchPath + '::' + self.options.pathDepth
         });
-      }else if (self.pattern.browsing) {
+      } else if (self.pattern.browsing) {
         criterias.push({
           i: 'path',
-          o: 'plone.app.querystring.operation.string.path',
+          o: self.options.pathOperator,
           v: self.getCurrentPath() + '::' + self.options.pathDepth
         });
       }
-      criterias = criterias.concat(options.additionalCriterias);
+      criterias = criterias.concat(searchOptions.additionalCriterias);
       return criterias;
-    };
-
-    self.getBatch = function(page) {
-      if (!page) {
-        page = 1;
-      }
-      return {
-        page: page,
-        size: self.options.batchSize
-      };
-    };
-
-    self.selectAjax = function() {
-      return {
-        url: self.options.vocabularyUrl,
-        dataType: 'JSON',
-        quietMillis: 100,
-        data: function(term, page) {
-          return self.getQueryData(term, page);
-        },
-        results: function (data, page) {
-          var more = (page * 10) < data.total; // whether or not there are more results available
-          // notice we return the value of more so Select2 knows if more results can be loaded
-          return {results: data.results, more: more};
-        }
-      };
-    };
-
-    self.getUrl = function() {
-      var url = self.options.vocabularyUrl;
-      if (url.indexOf('?') === -1) {
-        url += '?';
-      } else {
-        url += '&';
-      }
-      return url + $.param(self.getQueryData());
     };
 
     self.getQueryData = function(term, page) {
@@ -164,11 +132,40 @@ define('mockup-utils',[
       return data;
     };
 
+    self.getUrl = function() {
+      var url = self.options.vocabularyUrl;
+      if (url.indexOf('?') === -1) {
+        url += '?';
+      } else {
+        url += '&';
+      }
+      return url + $.param(self.getQueryData());
+    };
+
+    self.selectAjax = function() {
+      return {
+        url: self.options.vocabularyUrl,
+        dataType: 'JSON',
+        quietMillis: 100,
+        data: function(term, page) {
+          return self.getQueryData(term, page);
+        },
+        results: function(data, page) {
+          var more = (page * 10) < data.total; // whether or not there are more results available
+          // notice we return the value of more so Select2 knows if more results can be loaded
+          return {
+            results: data.results,
+            more: more
+          };
+        }
+      };
+    };
+
     self.search = function(term, operation, value, callback, useBaseCriteria, type) {
       if (useBaseCriteria === undefined) {
         useBaseCriteria = true;
       }
-      if(type === undefined){
+      if (type === undefined) {
         type = 'GET';
       }
       var criteria = [];
@@ -181,7 +178,9 @@ define('mockup-utils',[
         v: value
       });
       var data = {
-        query: JSON.stringify({ criteria: criteria }),
+        query: JSON.stringify({
+          criteria: criteria
+        }),
         attributes: JSON.stringify(self.options.attributes)
       };
       $.ajax({
@@ -196,7 +195,7 @@ define('mockup-utils',[
     return self;
   };
 
-  var Loading = function(options){
+  var Loading = function(options) {
     /*
      * Options:
      *   backdrop(pattern): if you want to have the progress indicator work
@@ -209,30 +208,30 @@ define('mockup-utils',[
       backdrop: null,
       zIndex: 10005 // can be a function
     };
-    if(!options){
+    if (!options) {
       options = {};
     }
     self.options = $.extend({}, defaults, options);
 
-    self.init = function(){
+    self.init = function() {
       self.$el = $('.' + self.className);
-      if(self.$el.length === 0){
+      if (self.$el.length === 0) {
         self.$el = $('<div><div></div></div>');
         self.$el.addClass(self.className).hide().appendTo('body');
       }
     };
 
-    self.show = function(closable){
+    self.show = function(closable) {
       self.init();
       self.$el.show();
       var zIndex = self.options.zIndex;
       if (typeof(zIndex) === 'function') {
         zIndex = Math.max(zIndex(), 10005);
-      }else{
+      } else {
         // go through all modals and backdrops and make sure we have a higher
         // z-index to use
         zIndex = 10005;
-        $('.plone-modal-wrapper,.plone-modal-backdrop').each(function(){
+        $('.plone-modal-wrapper,.plone-modal-backdrop').each(function() {
           zIndex = Math.max(zIndex, $(this).css('zIndex') || 10005);
         });
         zIndex += 1;
@@ -250,7 +249,7 @@ define('mockup-utils',[
       }
     };
 
-    self.hide = function(){
+    self.hide = function() {
       self.init();
       self.$el.hide();
     };
@@ -258,91 +257,125 @@ define('mockup-utils',[
     return self;
   };
 
-  var generateId = function(prefix){
+  var getAuthenticator = function() {
+    var $el = $('input[name="_authenticator"]');
+    if ($el.length === 0) {
+      $el = $('a[href*="_authenticator"]');
+      if ($el.length > 0) {
+        return $el.attr('href').split('_authenticator=')[1];
+      }
+      return '';
+    } else {
+      return $el.val();
+    }
+  };
+
+  var generateId = function(prefix) {
     if (prefix === undefined) {
       prefix = 'id';
     }
     return prefix + (Math.floor((1 + Math.random()) * 0x10000)
-        .toString(16).substring(1));
+      .toString(16).substring(1));
+  };
+
+  var setId = function($el, prefix) {
+    if (prefix === undefined) {
+      prefix = 'id';
+    }
+    var id = $el.attr('id');
+    if (id === undefined) {
+      id = generateId(prefix);
+    } else {
+      /* hopefully we don't screw anything up here... changing the id
+       * in some cases so we get a decent selector */
+      id = id.replace(/\./g, '-');
+    }
+    $el.attr('id', id);
+    return id;
+  };
+
+  var getWindow = function() {
+    var win = window;
+    if (win.parent !== window) {
+      win = win.parent;
+    }
+    return win;
+  };
+
+  var parseBodyTag = function(txt) {
+    return $((/<body[^>]*>((.|[\n\r])*)<\/body>/im).exec(txt)[0]
+      .replace('<body', '<div').replace('</body>', '</div>')).eq(0).html();
+  };
+
+  var featureSupport = {
+    /* Well tested feature support for things we use in mockup.
+     * All gathered from: http://diveintohtml5.info/everything.html
+     * Alternative to using some form of modernizr.
+     */
+    dragAndDrop: function() {
+      return 'draggable' in document.createElement('span');
+    },
+    fileApi: function() {
+      return typeof FileReader != 'undefined'; // jshint ignore:line
+    },
+    history: function() {
+      return !!(window.history && window.history.pushState);
+    }
+  };
+
+  var bool = function(val) {
+    if (typeof val === 'string') {
+      val = $.trim(val).toLowerCase();
+    }
+    return ['false', false, '0', 0, '', undefined, null].indexOf(val) === -1;
+  };
+
+  var escapeHTML = function(val) {
+    return $('<div/>').text(val).html();
+  };
+
+  var removeHTML = function(val) {
+    return val.replace(/<[^>]+>/ig, "");
   };
 
   return {
+    bool: bool,
+    escapeHTML: escapeHTML,
+    removeHTML: removeHTML,
+    featureSupport: featureSupport,
     generateId: generateId,
-    parseBodyTag: function(txt) {
-      return $((/<body[^>]*>((.|[\n\r])*)<\/body>/im).exec(txt)[0]
-          .replace('<body', '<div').replace('</body>', '</div>')).eq(0).html();
-    },
-    setId: function($el, prefix) {
-      if (prefix === undefined) {
-        prefix = 'id';
-      }
-      var id = $el.attr('id');
-      if (id === undefined) {
-        id = generateId(prefix);
-      } else {
-        /* hopefully we don't screw anything up here... changing the id
-         * in some cases so we get a decent selector */
-        id = id.replace(/\./g, '-');
-      }
-      $el.attr('id', id);
-      return id;
-    },
-    bool: function(val) {
-      if (typeof val === 'string') {
-        val = $.trim(val).toLowerCase();
-      }
-      return ['true', true, 1].indexOf(val) !== -1;
-    },
-    QueryHelper: QueryHelper,
+    getAuthenticator: getAuthenticator,
+    getWindow: getWindow,
     Loading: Loading,
-    // provide default loader
-    loading: new Loading(),
-    getAuthenticator: function() {
-      var $el = $('input[name="_authenticator"]');
-      if($el.length === 0){
-        $el = $('a[href*="_authenticator"]');
-        if($el.length > 0){
-          return $el.attr('href').split('_authenticator=')[1];
-        }
-        return '';
-      }else{
-        return $el.val();
-      }
-    },
-    featureSupport: {
-      /*
-        well tested feature support for things we use in mockup.
-        All gathered from: http://diveintohtml5.info/everything.html
-        Alternative to using some form of modernizr.
-      */
-      dragAndDrop: function(){
-        return 'draggable' in document.createElement('span');
-      },
-      fileApi: function(){
-        return typeof FileReader != 'undefined'; // jshint ignore:line
-      },
-      history: function(){
-        return !!(window.history && window.history.pushState);
-      }
-    }
+    loading: new Loading(),  // provide default loader
+    parseBodyTag: parseBodyTag,
+    QueryHelper: QueryHelper,
+    setId: setId
   };
 });
 
 require(['jquery', 'mockup-utils'], function($, utils) {
 
-	var portal_url = $('body').attr('data-portal-url');
-    $(document).on('click', '.jaz-shop-add', function(e) {
-        e.preventDefault();
-        var uid = $(this).attr('data-uid');
-        $.post(portal_url + '/shopping-cart', {
-        	'add': uid,
-        	'_authenticator': utils.getAuthenticator()
-        }, function(data) {
-          $('.jaz-shop-cart-wrapper').replaceWith(data);
-        }, 'html');
-    });
-
+    if ( $('body').hasClass('template-review-cart') ) {
+        // hide my cart on the review-cart view to prevent 
+        // potential inconsistencies if the cart is updated in this view
+        $('.jaz-shop-cart-wrapper').hide()
+    }
+    else {
+        $('.jaz-shop-cart-wrapper').show()
+	    var portal_url = $('body').attr('data-portal-url');
+        $(document).on('click', '.jaz-shop-add', function(e) {
+            e.preventDefault();
+            var uid = $(this).attr('data-uid');
+            $.post(portal_url + '/shopping-cart', {
+            	'add': uid,
+            	'_authenticator': utils.getAuthenticator()
+            }, function(data) {
+              $('.jaz-shop-cart-wrapper').replaceWith(data);
+            }, 'html');
+        });
+    }
 });
 
-define("/Users/davisagli/Work/jazkarta.com/src/jazkarta.shop/jazkarta/shop/browser/static/shop.js", function(){});
+define("/home/witek/dev/mex_sprint/zinstance/src/jazkarta.shop/jazkarta/shop/browser/static/shop.js", function(){});
 
--- jazkarta/shop/browser/static/shop.js
+++ jazkarta/shop/browser/static/shop.js
@@ -1,15 +1,22 @@
 require(['jquery', 'mockup-utils'], function($, utils) {
 
-	var portal_url = $('body').attr('data-portal-url');
-    $(document).on('click', '.jaz-shop-add', function(e) {
-        e.preventDefault();
-        var uid = $(this).attr('data-uid');
-        $.post(portal_url + '/shopping-cart', {
-        	'add': uid,
-        	'_authenticator': utils.getAuthenticator()
-        }, function(data) {
-          $('.jaz-shop-cart-wrapper').replaceWith(data);
-        }, 'html');
-    });
-
+    if ( $('body').hasClass('template-review-cart') ) {
+        // hide my cart on the review-cart view to prevent 
+        // potential inconsistencies if the cart is updated in this view
+        $('.jaz-shop-cart-wrapper').hide()
+    }
+    else {
+        $('.jaz-shop-cart-wrapper').show()
+	    var portal_url = $('body').attr('data-portal-url');
+        $(document).on('click', '.jaz-shop-add', function(e) {
+            e.preventDefault();
+            var uid = $(this).attr('data-uid');
+            $.post(portal_url + '/shopping-cart', {
+            	'add': uid,
+            	'_authenticator': utils.getAuthenticator()
+            }, function(data) {
+              $('.jaz-shop-cart-wrapper').replaceWith(data);
+            }, 'html');
+        });
+    }
 });
