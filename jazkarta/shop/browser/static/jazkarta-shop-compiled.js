059467a97651466ae7eeeb2e83e51cf244cf5ce9
David Glick <david@glicksoftware.com>
2017-05-05 21:16:12 -0500

    Fix js race condition

--- jazkarta/shop/browser/static/jazkarta-shop-compiled.js
+++ jazkarta/shop/browser/static/jazkarta-shop-compiled.js
@@ -330,23 +330,24 @@ define('mockup-utils',[
 
 require(['jquery', 'mockup-utils'], function($, utils) {
 
-	var portal_url = $('body').attr('data-portal-url');
-
     $(document).ready(function () {
+        var portal_url = $('body').attr('data-portal-url');
+
         $.get(portal_url + '/shopping-cart', function (data) {
             $('.jaz-shop-cart-wrapper').replaceWith(data);
         }, 'html');
-    });
 
-    $(document).on('click', '.jaz-shop-add', function(e) {
-        e.preventDefault();
-        var uid = $(this).attr('data-uid');
-        $.post(portal_url + '/shopping-cart', {
-        	'add': uid,
-        	'_authenticator': utils.getAuthenticator()
-        }, function(data) {
-            $('.jaz-shop-cart-wrapper').replaceWith(data);
-        }, 'html');
+        $(document).on('click', '.jaz-shop-add', function(e) {
+            e.preventDefault();
+            var uid = $(this).attr('data-uid');
+            $.post(portal_url + '/shopping-cart', {
+                'add': uid,
+                '_authenticator': utils.getAuthenticator()
+            }, function(data) {
+                $('.jaz-shop-cart-wrapper').replaceWith(data);
+            }, 'html');
+        });
+
     });
 
 });
