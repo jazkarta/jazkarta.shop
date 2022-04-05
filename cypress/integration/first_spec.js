ba3db525a90b511ea2d4f01f8ed83be51fe9adad
Silvio Tomatis <silviot@gmail.com>
2022-04-05 20:51:51 +0200

    Use ZEO for docker-compose to ease development

--- cypress/integration/first_spec.js
+++ cypress/integration/first_spec.js
@@ -5,7 +5,12 @@ before(() => {
         cy.contains("Install").click();
     });
     cy.contains("Installed jazkarta.shop!");
-    cy.contains("Jazkarta Shop Settings").click()
+    
+    cy.visit("http://localhost:8080/Plone/dexterity-types/Document/@@behaviors");
+    cy.get('input[name="form.widgets.jazkarta.shop.interfaces.IProduct:list"]').check();
+    cy.get('input[name="form.widgets.jazkarta.shop.interfaces.IRelatedProducts:list"]').check();
+    cy.contains("Save").click();
+    // cy.contains("Jazkarta Shop Settings").click()
 });
 describe('An order on a shop', () => {
     it('performs an order on the site', () => {
