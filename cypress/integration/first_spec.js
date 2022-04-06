44134c04131415c5c05edbba4fae0e97cdddb737
Silvio Tomatis <silviot@gmail.com>
2022-04-06 10:21:36 +0200

    Smoke test for order listing

--- cypress/integration/first_spec.js
+++ cypress/integration/first_spec.js
@@ -4,10 +4,9 @@ before(() => {
     cy.get('input[name="form.widgets.jazkarta.shop.interfaces.IProduct:list"]').check();
     cy.get('input[name="form.widgets.jazkarta.shop.interfaces.IRelatedProducts:list"]').check();
     cy.contains("Save").click();
-    // cy.contains("Jazkarta Shop Settings").click()
 });
-describe('An order on a shop', () => {
-    it('performs an order on the site', () => {
-        console.log("foo")
+describe('Admin operations', () => {
+    it('can view an empty order list', () => {
+        cy.visit("http://localhost:8080/Plone/@@jazkarta-shop-orders");
     })
 })
