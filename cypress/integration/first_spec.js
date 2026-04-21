dd7e470cb9145e6a25aedbdd7920c377a63708b9
Alec Mitchell <alecpm@gmail.com>
2026-04-20 18:40:46 -0700

    Update version number and fix browser test issue

--- cypress/integration/first_spec.js
+++ cypress/integration/first_spec.js
@@ -1,9 +1,5 @@
 before(() => {
     cy.ploneLoginAsRole("Manager");
-    // Enable product behavior on the `Document` content type
-    cy.visit("http://localhost:8080/Plone/dexterity-types/Document/@@behaviors");
-    cy.getByLabel('Jazkarta Shop Product').check()
-    cy.contains("Save").click();
     // Configure the Stripe processor
     cy.visit("http://localhost:8080/Plone/@@jazkarta-shop-settings");
     cy.getByLabel('Payment Processor').select('Stripe');
@@ -70,36 +66,11 @@ const createProduct = function (options = {}) {
         title = 'Product',
         price = '5',
         description = 'A purchasable product',
-        text = 'You can buy me!'
     } = options;
-    cy.visit("http://localhost:8080/Plone/++add++Document");
+    cy.visit("http://localhost:8080/Plone/++add++jazkarta.shop.product");
     cy.getByLabel('Title').type(title);
     cy.getByLabel('Summary').type(description);
-    getIframeBody('IRichTextBehavior-text').type(text)
     cy.contains("Shop").click();
     cy.getByLabel('Unit Price').clear().type(price);
     cy.contains("Save").click();
-}
-
-
-const getIframeDocument = (fieldName) => {
-    return cy
-        .get(`#formfield-form-widgets-${fieldName} iframe`)
-        // Cypress yields jQuery element, which has the real
-        // DOM element under property "0".
-        // From the real DOM iframe element we can get
-        // the "document" element, it is stored in "contentDocument" property
-        // Cypress "its" command can access deep properties using dot notation
-        // https://on.cypress.io/its
-        .its('0.contentDocument').should('exist')
-}
-
-const getIframeBody = (fieldName) => {
-    // get the document
-    return getIframeDocument(fieldName)
-        // automatically retries until body is loaded
-        .its('body').should('not.be.undefined')
-        // wraps "body" DOM element to allow
-        // chaining more Cypress commands, like ".find(...)"
-        .then(cy.wrap)
 }
